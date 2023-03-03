956e335fbc560b58aade63f4f4ea8147e5b10455
Umar Hussain <ufhussai@uwaterloo.ca>
2023-03-03 11:01:28 -0500

    moved deep learning code out of backend

--- app/frontend/app/src/FireFly/Common/button.tsx
+++
@@ -1,8 +0,0 @@
-// Change button color.
-export const changeColor = (e: any, color:string) => {
-    e.target.style.color = color
-}
-
-export const changeBackground = (e: any, color:string) => {
-    e.target.style.background = color
-}
